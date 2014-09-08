927589823200a3086dc41892821be649f573409e
Julian Montez <julian.montez@gmail.com>
2014-09-07 19:24:53 -0500

    Change CLI arguments

--- test_runner/executable.py
+++ test_runner/executable.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argh
 
 from .environments import Environment
@@ -7,14 +8,14 @@ from .utils import cleanup, Reporter
 LOG = Reporter(__name__).setup()
 
 
-def main(endpoint, username='admin', password='secrete', test_path='api'):
+def main(username, password, endpoint, repo_dir='/opt/tempest',
+         test_path='api'):
     environment = Environment(username, password, endpoint)
 
     with cleanup(environment):
         environment.build()
-        framework = Tempest(environment, repo_dir='/opt/tempest',
-                            test_path=test_path)
-        results = framework.run_tests()
+        results = Tempest(environment, repo_dir, test_path).run_tests()
         LOG.info('Results: {0}'.format(results))
+
 
 argh.dispatch_command(main)
