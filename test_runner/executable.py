49f0123625538a8ae2f08e9fb98f99fe137eca74
Julian Montez <julian.montez@gmail.com>
2014-10-16 16:42:26 -0500

    Carry over staged environment into the Tempest run

--- test_runner/executable.py
+++ test_runner/executable.py
@@ -33,8 +33,8 @@ def main():
     environment = Environment(args.username, args.password, args.endpoint)
 
     with cleanup(environment):
-        environment.build()
-        Tempest(environment).run()
+        stage = environment.build()
+        Tempest(stage).run()
 
 
 if __name__ == '__main__':
